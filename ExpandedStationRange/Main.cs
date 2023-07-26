e71b2ea7fd6c45eb858c408f9c463c9d8f0a0094
Niko Fox <niko@fauxnik.me>
2023-07-26 12:17:39 -0700

    make mod behavior compatible with Simulator
    Simulator introduced a main menu, so the range values must now be rewritten every time the world finishes loading.
    Initial values are now saved per station rather than one value for all stations.
    Now finds game components rather than patching methods for one-and-done functionality.
    Finding game components instead of patching methods also enables proper enabled state toggling.

--- ExpandedStationRange/Main.cs
+++ ExpandedStationRange/Main.cs
@@ -1,108 +1,140 @@
 ﻿using System;
-using System.Reflection;
-using HarmonyLib;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
 using UnityModManagerNet;
 
 namespace ExpandedStationRange;
 
 public static class Main
 {
-#pragma warning disable CS8618 // ModEntry will be set during Load
-	private static UnityModManager.ModEntry ModEntry;
-#pragma warning restore CS8618
-	private static Harmony? harmony;
-	private static float initialDistanceRegular = 0f;
-	private static float initialDistanceAnyJobTaken = 0f;
+	const float MINIMUM_SQR_RANGE = 4_000_000f;
+
+	private static List<StationJobGenerationRange>? rangeComponents;
+	private static readonly Dictionary<string, float> initialDistanceRegular = new();
+	private static readonly Dictionary<string, float> initialDistanceAnyJobTaken = new();
+#if RELEASE
+#pragma warning disable CS0649 // It's expected that LogDebug will never be assigned in Release builds
+#endif
+	private static Action<string>? LogDebug;
+#pragma warning restore CS0649
+	private static Action? patchOnLoadingFinished;
 
 	// Unity Mod Manage Wiki: https://wiki.nexusmods.com/index.php/Category:Unity_Mod_Manager
 	private static bool Load(UnityModManager.ModEntry modEntry)
 	{
-		ModEntry = modEntry;
-		return DoPatch();
+#if DEBUG
+		LogDebug = modEntry.Logger.Log;
+#endif
+		modEntry.OnToggle = OnToggle;
+		return true;
 	}
 
-	private static bool OnToggle(UnityModManager.ModEntry modEntry, bool value)
+	private static bool OnToggle(UnityModManager.ModEntry modEntry, bool isTogglingOn)
 	{
-		modEntry.Enabled = value;
+		LogDebug?.Invoke($"Toggle {(isTogglingOn ? "on" : "off")} requested for {modEntry.Info.DisplayName}");
+		bool result = true;
 
-		if (value)
+		if (isTogglingOn)
 		{
-			return DoPatch();
+			patchOnLoadingFinished ??= () => DoPatch(modEntry);
+			WorldStreamingInit.LoadingFinished += patchOnLoadingFinished;
+			if (WorldStreamingInit.IsLoaded)
+			{
+				result = DoPatch(modEntry);
+			}
+		}
+		else
+		{
+			if (patchOnLoadingFinished != null)
+			{
+				WorldStreamingInit.LoadingFinished -= patchOnLoadingFinished;
+			}
+			if (WorldStreamingInit.IsLoaded)
+			{
+				result = DoUnpatch(modEntry);
+			}
 		}
 
-		return DoUnpatch();
+		return result;
 	}
 
-	private static bool DoPatch()
+	private static bool DoPatch(UnityModManager.ModEntry modEntry)
 	{
+		LogDebug?.Invoke($"Doing patch for {modEntry.Info.DisplayName}");
+
 		try
 		{
-			harmony ??= new Harmony(ModEntry.Info.Id);
-			harmony.PatchAll(Assembly.GetExecutingAssembly());
+			rangeComponents = UnityEngine.Object.FindObjectsOfType<StationJobGenerationRange>().ToList();
+			LogDebug?.Invoke($"Found {rangeComponents.Count} StationJobGenerationRange components");
+
+			foreach (var rangeComponent in rangeComponents)
+			{
+				string stationName = rangeComponent.gameObject.name;
+				// Backup existing values before overwriting
+				if (!initialDistanceRegular.ContainsKey(stationName))
+				{
+					initialDistanceRegular.Add(stationName, rangeComponent.destroyGeneratedJobsSqrDistanceRegular);
+				}
+				if (!initialDistanceAnyJobTaken.ContainsKey(stationName))
+				{
+					initialDistanceAnyJobTaken.Add(stationName, rangeComponent.destroyGeneratedJobsSqrDistanceAnyJobTaken);
+				}
+
+				// Only overwrite the job taken range if it's less than our minimum
+				if (rangeComponent.destroyGeneratedJobsSqrDistanceAnyJobTaken < MINIMUM_SQR_RANGE)
+				{
+					rangeComponent.destroyGeneratedJobsSqrDistanceAnyJobTaken = MINIMUM_SQR_RANGE;
+				}
+				rangeComponent.destroyGeneratedJobsSqrDistanceRegular = rangeComponent.destroyGeneratedJobsSqrDistanceAnyJobTaken;
+			}
 		}
 		catch (Exception ex)
 		{
-			ModEntry.Logger.LogException($"Failed to load {ModEntry.Info.DisplayName}:", ex);
-			harmony?.UnpatchAll();
-			harmony = null;
+			modEntry.Logger.LogException($"Failed to load {modEntry.Info.DisplayName}:", ex);
+			modEntry.Enabled = false;
+			rangeComponents = null;
 			return false;
 		}
 
 		return true;
 	}
 
-	private static bool DoUnpatch()
+	private static bool DoUnpatch(UnityModManager.ModEntry modEntry)
 	{
-		if (harmony == null)
+		LogDebug?.Invoke($"Doing unpatch for {modEntry.Info.DisplayName}");
+
+		if (rangeComponents == null)
 		{
-			// The patch operation must have not succeeded if the harmony field is null.
+			// The patch operation must have not succeeded if the reangeComponents field is null.
 			// Return as if it's been unpatched since it was never patched to start.
 			return true;
 		}
 
 		try
 		{
-			harmony.UnpatchAll();
+			foreach (var rangeComponent in rangeComponents)
+			{
+				string stationName = rangeComponent.gameObject.name;
+				if (!initialDistanceRegular.ContainsKey(stationName))
+				{
+					throw new Exception($"Couldn't find a cached initial regular distance for ${stationName}");
+				}
+				if (!initialDistanceAnyJobTaken.ContainsKey(stationName))
+				{
+					throw new Exception($"Couldn't find a cached initial job taken distance for ${stationName}");
+				}
+
+				rangeComponent.destroyGeneratedJobsSqrDistanceAnyJobTaken = initialDistanceAnyJobTaken[stationName];
+				rangeComponent.destroyGeneratedJobsSqrDistanceRegular = initialDistanceRegular[stationName];
+			}
 		}
 		catch (Exception ex)
 		{
-			ModEntry.Logger.LogException($"Failed to unload {ModEntry.Info.DisplayName}:", ex);
+			modEntry.Logger.LogException($"Failed to unload {modEntry.Info.DisplayName}:", ex);
 			return false;
 		}
 
 		return true;
-	}
-
-	[HarmonyPatch(typeof(StationJobGenerationRange))]
-	[HarmonyPatchAll]
-	class StationJobGenerationRange_AllMethods_Patch
-	{
-		static void Prefix(StationJobGenerationRange __instance)
-		{
-			// backup existing values before overwriting
-			if (initialDistanceRegular < 1f)
-			{
-				initialDistanceRegular = __instance.destroyGeneratedJobsSqrDistanceRegular;
-			}
-			if (initialDistanceAnyJobTaken < 1f)
-			{
-				initialDistanceAnyJobTaken = __instance.destroyGeneratedJobsSqrDistanceAnyJobTaken;
-			}
-
-			if (ModEntry.Active)
-			{
-				if (__instance.destroyGeneratedJobsSqrDistanceAnyJobTaken < 4000000f)
-				{
-					__instance.destroyGeneratedJobsSqrDistanceAnyJobTaken = 4000000f;
-				}
-				__instance.destroyGeneratedJobsSqrDistanceRegular =
-					__instance.destroyGeneratedJobsSqrDistanceAnyJobTaken;
-			}
-			else
-			{
-				__instance.destroyGeneratedJobsSqrDistanceRegular = initialDistanceRegular;
-				__instance.destroyGeneratedJobsSqrDistanceAnyJobTaken = initialDistanceAnyJobTaken;
-			}
-		}
 	}
 }
